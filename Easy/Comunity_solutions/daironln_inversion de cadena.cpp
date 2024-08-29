5add58a8253497cf0b5bc474fd0f58b1d4adee23
Dairon Luis <159643939+daironln@users.noreply.github.com>
2024-08-29 13:31:04 -0400

    Create daironln_inversion de cadena.cpp

---
+++ Easy/Comunity_solutions/daironln_inversion de cadena.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+
+typedef long long ll;
+
+using namespace std;
+
+const int mod = 1e9+7;
+
+auto count_vocals(string) -> int;
+
+auto main() -> signed{
+
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(nullptr);
+
+
+    string s;
+    cin >> s;
+
+    reverse(s.begin(), s.end());
+    cout << s << endl;
+
+}
+
